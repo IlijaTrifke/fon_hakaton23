4abaed1307ab2d1bef49d4e79501373a9e3c74b6
Petrovic-Luka <lukapetrovic00@gmail.com>
2023-01-22 21:05:57 +0100

    osnovni izgled i responzivnost

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,10 @@ import React from "react";
 import Agenda from "./components/agenda/Agenda";
 import Statistika from "./components/statistika/Statistika";
 import Pocetna from "./components/pocetna/Pocetna";
+import Nagrade from "./components/nagrade/Nagrade";
 
 const Home = () => {
-  return <Statistika />;
+  return <Nagrade />;
 };
 
 export default Home;
