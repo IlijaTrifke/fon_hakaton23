b39a4490ae0a9898de79bafbde1a6996b411eab5
Aleksndar <acajoksimovic70@gmail.com>
2023-01-26 19:32:09 +0100

    Uradjena responzivnost i oblici

--- src/components/slikeOrg/SlikeOrg.jsx
+++ src/components/slikeOrg/SlikeOrg.jsx
@@ -1,5 +1,25 @@
 import React from "react";
-import "./SlikeOrg.scss";
+import "./slikeOrg.scss";
+import slika from "./god.png";
+
 export const SlikeOrg = () => {
-  return <div>SlikeOrg</div>;
+  return (
+    <div className="org-section">
+      <h2 className="org-header">Organizacioni tim</h2>
+      <div className="org-content">
+        <div className="org-text">
+          <h2 className="org-h2">LOREM IPSUM</h2>
+          <p className="org-p">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor non
+            iure, quidem ea, reiciendis voluptate tempore libero alias
+            repudiandae atque voluptatibus reprehenderit omnis distinctio
+            repellat pariatur similique? Voluptate, dolor quia!
+          </p>
+        </div>
+        <div className="org-photo-hold">
+          <img className="org-photo" src={slika} alt="" srcset="" />
+        </div>
+      </div>
+    </div>
+  );
 };
