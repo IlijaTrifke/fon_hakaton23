62d8f6eb677cd6b9883eeb5ebaf04128b6d0517d
pozitiva <ivadjokovic2@gmail.com>
2023-01-29 02:32:13 +0100

    uradjene osnove

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
-import Agenda from "./components/agenda/Agenda";
-import Statistika from "./components/statistika/Statistika";
-import Pocetna from "./components/pocetna/Pocetna";
-import FAQ from "./components/faq/Faq";
-import Partneri from "./components/partneri/Partneri";
-import Nagrade from "./components/nagrade/Nagrade";
+// import Agenda from "./components/agenda/Agenda";
+// import Statistika from "./components/statistika/Statistika";
+// import Pocetna from "./components/pocetna/Pocetna";
+// import FAQ from "./components/faq/Faq";
+// import Partneri from "./components/partneri/Partneri";
+// import Nagrade from "./components/nagrade/Nagrade";
+import Ohakatonu from "./components/o-hakatonu/Ohakatonu";
 
-const Home = () => {
+function Home() {
   return (
     <>
-      <FAQ />
-      <Agenda />;
-      <Nagrade />;
-      <Partneri />
+      {/* <FAQ /> */}
+      {/* <Agenda />;<Nagrade />;<Partneri />; */}
+      <Ohakatonu />
     </>
   );
-};
+}
 
 export default Home;
