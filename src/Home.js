245eaba5ba6d5b00ad3227e570aae7280f0a0356
leki0 <aleksastancic9@gmail.com>
2023-01-30 01:20:07 +0100

    eo

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Iskustva from "./components/iskustva/Iskustva";
-// import Partneri from "./components/partneri/Partneri";
+
+import Partneri from "./components/partneri/Partneri";
 
 const Home = () => {
-  return <Iskustva />;
-  // return <Partneri />;
+  return <Partneri />;
 };
 
 export default Home;
