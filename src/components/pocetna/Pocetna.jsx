0752f6260ede14a950d88fd261a6f41396438963
Ilija <gtrifunovic05@gmail.com>
2023-02-01 12:32:19 +0100

    zavrsio pocetnu

--- src/components/pocetna/Pocetna.jsx
+++ src/components/pocetna/Pocetna.jsx
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./pocetna.scss";
 import logo from "../../images/FH_Logo.png";
 
 export default function Pocetna() {
+  const [timer, setTimer] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTimer(() => getTime(new Date("Feb 30, 2023 23:59:59").getTime()));
+    }, 1000);
+  });
+
+  function getTime(countDownDate) {
+    let now = new Date().getTime();
+
+    let distance = countDownDate - now;
+
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (days < 10) {
+      days = "0" + days;
+    }
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+
+    return `${days}:${hours}:${minutes}:${seconds}`;
+  }
+
   return (
     <div className="pocetna-section">
       <img src={logo} alt="Fon hakaton logo" className="pocetna-logo" />
@@ -18,7 +54,13 @@ export default function Pocetna() {
           <div className="pocetna-arrow" />
         </div>
         <div className="pocetna-bottom-middle">
-          <h1>Ostalo je još:</h1>
+          <h1 className="pocetna-middle-text">Ostalo je još:</h1>
+          <div className="pocetna-timer">
+            {/* <div className="timer-text">
+              <div className="timer-span">PRIJAVE SU ZATVORENE!</div>
+            </div> */}
+            {timer}
+          </div>
           <div className="pocetna-date-box"></div>
         </div>
         <div className="pocetna-arrows">
@@ -28,6 +70,8 @@ export default function Pocetna() {
           <div className="pocetna-arrow" />
         </div>
       </div>
+      <div className="blue-ball-blur" />
+      <div className="purple-rectangle-blur" />
     </div>
   );
 }
