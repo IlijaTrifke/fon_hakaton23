59ca034f680af0903316e2be01583afd2675747b
leki0 <aleksastancic9@gmail.com>
2023-02-04 19:49:24 +0100

    d

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Iskustva from "./components/iskustva/Iskustva";
+// import SlikeTima from "./components/slike_tima/Slike_tima.jsx";
 
 const Home = () => {
   return <Iskustva />;
+  // return <SlikeTima />;
 };
 
 export default Home;
