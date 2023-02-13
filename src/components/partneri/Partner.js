0d207ab57d661213b8b3ee788d277b4769aea4ce
Ilija <gtrifunovic05@gmail.com>
2023-02-13 23:56:04 +0100

    uredio ostatak

--- src/components/partneri/Partner.js
+++ src/components/partneri/Partner.js
@@ -3,15 +3,8 @@ import "./partneri.scss";
 
 const Partner = (props) => {
   return (
-    <div
-      className="partner"
-      style={{
-        width: props.width,
-        height: props.height,
-        backgroundColor: props.pozadina,
-      }}
-    >
-      <div className="ax-okvir-partneri">
+    <div className="partner">
+      <div id={props.id} className="ax-okvir-partneri">
         <a href={props.link} target="_blank" rel="noreferrer">
           <img src={props.partnerSlika} alt={props.partnerIme} />
         </a>
