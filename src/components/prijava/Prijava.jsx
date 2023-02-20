ce87aee6484bd82b06bc506ad3f5584d85fe6f30
Ilija <gtrifunovic05@gmail.com>
2023-02-20 13:00:44 +0100

    doradio partenre

--- src/components/prijava/Prijava.jsx
+++ src/components/prijava/Prijava.jsx
@@ -291,6 +291,12 @@ const Prijava = () => {
       {loading && <Loader />}
 
       <div class="pr-prijava">
+        <div className="balls-wrapper">
+          <div className="pr-blue-ball-blur" />
+          <div className="pr-purple-rectangle-blur" />
+          <div className="pr-blue-ball-blur2" />
+          <div className="pr-purple-rectangle-blur2" />
+        </div>
         <div class="pr-header">
           <h1 class="pr-h1">Prijava</h1>
           <HashLink to="/#pocetna" class="pr-exit-text">
