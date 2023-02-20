ddde5bd7813e5be2fb918d1e8b9c96f8e85ef146
pozitiva <ivadjokovic2@gmail.com>
2023-02-20 11:18:12 +0100

    ubacene slike

--- src/components/iskustva/DataSlider.jsx
+++ src/components/iskustva/DataSlider.jsx
@@ -1,40 +1,40 @@
 export const DataSlider = [
   {
     id: 0,
-    image: require("../../images/Blajvislav.jpg"),
+    image: require("./img/iva.jpeg"),
     name: "Iva Đoković",
     description:
-      "Kao prošlogodišnji koordinator FON Hakatona dobila sam jedinstvenu priliku da vidim kako izgleda organizovati ovako veliko takmičenje. Uvidela sam koliko je važno biti odgovoran za svoje postupke i kako izgleda kad drugi ljudi u timu zavise od tvog rada. Upoznala sam sjajne ljude iz partnerskih kompanija i stvorila važne poslovne veze koje će mi značiti u daljem karijernom razvoju.",
+      "„Kao prošlogodišnji koordinator FON Hakatona dobila sam jedinstvenu priliku da vidim kako izgleda organizovati ovako veliko takmičenje. Uvidela sam koliko je važno biti odgovoran za svoje postupke i kako izgleda kad drugi ljudi u timu zavise od tvog rada. Upoznala sam sjajne ljude iz partnerskih kompanija i stvorila važne poslovne veze koje će mi značiti u daljem karijernom razvoju.“",
   },
   {
     id: 1,
-    image: require("../../images/moze.JPG"),
+    image: require("./img/milos.jpg"),
     name: "Miloš Ninković",
     description:
-      "Bilo je veoma izazovno raditi preko 24 sata i rješavati probleme, ali zahvaljujući odličnoj atmosferi na takmičenju vrijeme je proletjelo. Osvojeno prvo mjesto na FON Hakatonu je pokazalo mom timu i meni da imamo dobru ideju i inspirisalo nas je da nastavimo sa razvojem naše aplikacije.",
+      "„Bilo je veoma izazovno raditi preko 24 sata i rješavati probleme, ali zahvaljujući odličnoj atmosferi na takmičenju vrijeme je proletjelo. Osvojeno prvo mjesto na FON Hakatonu je pokazalo mom timu i meni da imamo dobru ideju i inspirisalo nas je da nastavimo sa razvojem naše aplikacije.“",
   },
   {
     id: 2,
-    image: require("../../images/tea.png"),
+    image: require("./img/dusan.jpeg"),
     name: "Dušan Lazić, član tima 404!",
     description:
-      "Svaki trenutak na FON Hakatonu ću dugo pamtiti, a stečeno iskustvo ću primeniti na svakom budućem takmičenju zato što ovo takmičenje nije samo programerskog tipa, već je potrebno udružiti sve snage tima i iskoristiti sva moguća znanja, podrazumevajući maštovitost, kreativnost, praktičnost, kao i što veću snalažljivost. Može se reći da je takmičenje naporno, ali je to jedna od draži ovog takmičenja. Činjenica da smo bili jedini srednjoškolci u finalu takmičenja, nam je dala posebnu motivaciju da damo sve od sebe.",
+      "„Svaki trenutak na FON Hakatonu ću dugo pamtiti, a stečeno iskustvo ću primeniti na svakom budućem takmičenju zato što ovo takmičenje nije samo programerskog tipa, već je potrebno udružiti sve snage tima i iskoristiti sva moguća znanja, podrazumevajući maštovitost, kreativnost, praktičnost, kao i što veću snalažljivost. Može se reći da je takmičenje naporno, ali je to jedna od draži ovog takmičenja. Činjenica da smo bili jedini srednjoškolci u finalu takmičenja, nam je dala posebnu motivaciju da damo sve od sebe.“",
   },
   {
     id: 3,
-    image: require("../../images/ana.JPG"),
+    image: require("./img/gagi.jpeg"),
     name: "Dragoslav Janković",
 
     description:
-      "FON hakaton mi je pružio nezaboravno iskustvo i priliku da pokažem svoje programerske veštine pred uticajnim ljudima iz stručnog žirija. Shvatio sam da su dobra ideja, sloga u timu i upornost ključni aspekti koji vode ka dobrom rezultatu na takmičenju. Učestvovanje na hakatonu me je motivisalo da unapredim svoje veštine i da se oprobam u novim tehnologijama. Okruženje mladih ljudi koji su puni entuzijazma i pozitivne energije je učinilo da FON hakaton prevaziđe sva moja očekivanja.",
+      "„FON hakaton mi je pružio nezaboravno iskustvo i priliku da pokažem svoje programerske veštine pred uticajnim ljudima iz stručnog žirija. Shvatio sam da su dobra ideja, sloga u timu i upornost ključni aspekti koji vode ka dobrom rezultatu na takmičenju. Učestvovanje na hakatonu me je motivisalo da unapredim svoje veštine i da se oprobam u novim tehnologijama. Okruženje mladih ljudi koji su puni entuzijazma i pozitivne energije je učinilo da FON hakaton prevaziđe sva moja očekivanja.“",
   },
   {
     id: 4,
-    image: require("../../images/Jolobitch.jpg"),
+    image: require("./img/delegacija.JPG"),
     name: "tim Delegacija, pobednički tim FH 2022",
 
     description:
-      "Učestvovati na devetom FON Hakatonu za nas je bio veliki izazov. Najzad smo imali priliku da pokažemo sve što smo godinama učili i testiramo naše sposobnosti rada u timu pod pritiskom. Kod dobrog rešenja najbitnija je ideja, kojoj smo zato i mi posvetili najviše vremena. Razmišljali smo sa aspekta građanina koji želi da bude ekološki odgovoran ali da mu to ne oduzima previše vremena. Naša aplikacija omogućava svima da učitaju bilo koji proizvod i vide od kojih materijala je on napravljen, gde i kako može da se reciklira. Za 24 sata susreli smo se sa problemima koje smo kao tim, uz pomoć stručnih mentora, brzo i efikasno rešili. Udruženje studenata informatike FONIS i kompanija Levi9 su celokupno takmičenje organizovali na najbolji mogući način. Od trenutka kad smo poslali prijavu pa sve do proglašenja pobednika, imali smo sjajnu komunikaciju sa svima i zaista je FON Hakaton prevazišao naša očekivanja. Organizatori su bili tu za nas u svakom trenutku, a predstavnici kompanije Levi9 i zaposleni na Fakultetu organizacionih nauka su nam dali najbolje savete i smernice u kojem da razvijamo naše rešenje.",
+      "„Učestvovati na devetom FON Hakatonu za nas je bio veliki izazov. Najzad smo imali priliku da pokažemo sve što smo godinama učili i testiramo naše sposobnosti rada u timu pod pritiskom. Za 24 sata susreli smo se sa problemima koje smo kao tim, uz pomoć stručnih mentora, brzo i efikasno rešili. Od trenutka kad smo poslali prijavu pa sve do proglašenja pobednika, imali smo sjajnu komunikaciju sa svima i zaista je FON Hakaton prevazišao naša očekivanja.“",
   },
   // {
   //   id: 5,
